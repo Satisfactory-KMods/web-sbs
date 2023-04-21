633576bd4e813e9a5e50a7e99162225c7211380a
Oliver <olli143@live.de>
2023-04-21 11:09:01 +0200

    working on Blueprint Packs

---
+++ server/src/MongoDB/DB_BlueprintPacks.ts
@@ -0,0 +1,17 @@
+import * as mongoose     from "mongoose";
+import { IMO_Blueprint } from "../../../src/Shared/Types/MongoDB";
+
+const BlueprintSchema = new mongoose.Schema<IMO_Blueprint>( {
+	name: { type: String, required: true },
+	description: { type: String, required: true },
+	mods: { type: [ String ], required: true },
+	likes: { type: [ String ], required: true },
+	tags: { type: [ String ], required: true },
+	DesignerSize: { type: String, required: true },
+	owner: { type: String, required: true },
+	downloads: { type: Number, required: false, default: 0 },
+	blacklisted: { type: Boolean, required: false, default: false }
+}, { timestamps: true } );
+
+export default mongoose.model<IMO_Blueprint>( "SBS_Blueprints", BlueprintSchema );
+export { BlueprintSchema };
