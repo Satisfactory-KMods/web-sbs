b53ad96c320efd070149188c0c80cf3f79d8f718
Oliver <olli143@live.de>
2023-05-03 12:18:06 +0200

    remove old

--- old/server/src/MongoDB/DB_Tags.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from "mongoose";
-import { MO_Tag }    from "@shared/Types/MongoDB";
-
-const UserAccountSchema = new mongoose.Schema<MO_Tag>( {
-	DisplayName: { type: String, required: true }
-}, { timestamps: true } );
-
-export default mongoose.model<MO_Tag>( "SBS_Tags", UserAccountSchema );
-export { UserAccountSchema };
--- old/src/Shared/Enum/EDesignerSize.ts
+++
@@ -1,7 +0,0 @@
-export enum EDesignerSize {
-	mk1 = "4x4x4",
-	mk2 = "6x6x6",
-	mk3 = "8x8x8",
-	mk4 = "10x10x8",
-	mk5 = "12x12x6"
-}
