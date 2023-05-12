2492035bcf8d6314392385b5e01a3586366e295b
Kyri123 <olli143@live.de>
2023-05-12 17:16:19 +0200

    🐛 add missing import

--- server/src/trpc/server.ts
+++ server/src/trpc/server.ts
@@ -14,6 +14,7 @@ import {
 } from "@server/trpc/trpc";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { auth_blueprints } from "./routings/auth/blueprints";
+import { admin_tags } from "./routings/auth/tags";
 
 
 const publicRouter = router( {
