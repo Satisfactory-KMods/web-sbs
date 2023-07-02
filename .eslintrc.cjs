5552f86d85548f3bd17f51dfee6fedc0d344d329
Oliver <olli143@live.de>
2023-07-02 19:29:42 +0200

    add jsx-curly-brace-presence

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
 	},
 	plugins: ['react', 'import', 'jsx-a11y', 'react-hooks', '@typescript-eslint', 'prettier'],
 	rules: {
+		'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never', propElementValues: 'always' }],
 		'no-empty-pattern': 'off',
 		'@typescript-eslint/consistent-type-imports': 'error',
 		'prettier/prettier': [
